15b2bff1db01cc2dbb10c5dfdd9418ab80ec5dc3
juan torres <159671625+juan-torresf@users.noreply.github.com>
2025-03-23 20:01:20 -0600

    Update splines.py

--- splines.py
+++ splines.py
@@ -34,7 +34,7 @@ def sujeto(points=points,df=df):
     h = [0]+[points[i][0]-points[i-1][0]for i in range(1,len(points))]
     
     alpha[0] = 3*((points[1][1]-points[0][1])/h[1]-df(points[0][0]))
-    alpha[len(points)-1] = 3*(df(points[len(points)-1][0])-(points[len(points)-1][1]-points[len(points)-1-1][1])/h[len(points)-1])
+    alpha[len(points)-1] = 3*(df(points[len(points)-1][0])-(points[len(points)-1][1]-points[len(points)-2][1])/h[len(points)-1])
     for i in range(1,len(points)-1):
         alpha[i] = 3*((points[i+1][1]-points[i][1])/h[i+1]-(points[i][1]-points[i-1][1])/h[i])
     
